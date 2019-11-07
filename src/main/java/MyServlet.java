1b61eb18642ee314de84f23da83c6e83dc08ce1b
ricardomokhtari <ricardomokhtari@gmail.com>
2019-11-07 13:37:36 +0000

    Added h1

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -11,7 +11,7 @@ public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        resp.getWriter().write("Hi there");
+        resp.getWriter().write("<h1>Hi there</h1>");
     }
 
     @Override
