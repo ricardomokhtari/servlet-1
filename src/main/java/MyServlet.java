42fa1530395172b8c34bfdc78dd8d13f07b3ce59
ricardomokhtari <ricardomokhtari@gmail.com>
2019-11-07 13:32:46 +0000

    Test

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -11,7 +11,7 @@ public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        resp.getWriter().write("<p font-family = 'Century Gothic'>Hello World</p>");
+        resp.getWriter().write("Hi there");
     }
 
     @Override
