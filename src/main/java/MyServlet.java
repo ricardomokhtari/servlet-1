5d973908240f4da0331e19f39fa1929d02ab525a
ricardomokhtari <ricardomokhtari@gmail.com>
2019-11-07 13:27:01 +0000

    Added html

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -11,7 +11,7 @@ public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        resp.getWriter().write("Hello World");
+        resp.getWriter().write("<p>Hello World</p>");
     }
 
     @Override
