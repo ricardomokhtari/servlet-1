3dd8c89b67137bdf5f0b537e77d51be35b5445ea
ricardomokhtari <ricardomokhtari@gmail.com>
2019-11-13 15:45:59 +0000

    Added unit testing for Servlet

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 @WebServlet(urlPatterns={"/patients"},loadOnStartup = 1)
 public class MyServlet extends HttpServlet {
@@ -11,7 +12,7 @@ public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        resp.getWriter().write("<h1>Hi there</h1>");
+        resp.getWriter().write("Hello World");
     }
 
     @Override
---
+++ src/test/java/ServletTest.java
@@ -0,0 +1,38 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.logging.Logger;
+
+import static org.mockito.Mockito.when;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+
+public class ServletTest {
+    @Mock
+    HttpServletRequest request;
+    @Mock
+    HttpServletResponse response;
+    @Before
+    public void setUp() throws Exception{
+        MockitoAnnotations.initMocks(this);
+    }
+    @Test
+    public void testDoGet() throws IOException, ServletException {
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+        when(response.getWriter()).thenReturn(printWriter);
+        when(request.getServletPath()).thenReturn("/patients");
+        MyServlet myServlet = new MyServlet();
+        myServlet.doGet(request,response);
+        String output=stringWriter.getBuffer().toString();
+        Assert.assertThat(output, is(equalTo("Hello World")));
+    }
+}
