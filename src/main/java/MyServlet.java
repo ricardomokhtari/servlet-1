6df9f7af334115526f5ad1a05a351fa6d8de892d
ricardomokhtari <ricardomokhtari@gmail.com>
2019-12-08 09:52:29 +0000

    Changed POST/GET requests

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -9,25 +9,18 @@ import java.io.IOException;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
-@WebServlet(urlPatterns={"/patients"},loadOnStartup = 1)
+@WebServlet(urlPatterns={"/"},loadOnStartup = 1)
 public class MyServlet extends HttpServlet {
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
-        resp.getWriter().write("Hello World");
+        resp.setContentType("application/json");
+        resp.getWriter().write("GET Received");
     }
-
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String reqBody=req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-        //resp.setContentType("text/html");
-        //resp.getWriter().write("Thank you client!");
-        Gson gson = new Gson();
-        Patient p = gson.fromJson(reqBody,Patient.class);
-        resp.getWriter().write(p.getName());
-        resp.getWriter().write(p.getPhoneNum());
-        resp.getWriter().write(p.getId());
+        resp.setContentType("text/html");
+        resp.getWriter().write("POST Received");
+        System.out.println(reqBody);
     }
-
 }
