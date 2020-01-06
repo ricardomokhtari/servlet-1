275a5350fdfc8c6ffe6ec920265709d9ddf5c972
Ricardo Mokhtari <ricardomokhtari@gmail.com>
2020-01-06 22:06:24 +0000

    Added new patient functionality in doPost

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -14,27 +14,38 @@ import org.json.*;
 
 @WebServlet(urlPatterns={"/"},loadOnStartup = 1)
 public class MyServlet extends HttpServlet {
+    // function that handles GET requests from frontend
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // database URL
         String dbURL = "jdbc:postgresql://ec2-54-195-252-243.eu-west-1.compute.amazonaws.com:5432/din8m6nuaj2gb?ssl" +
                "=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
         try {
+            // try to connect to postgres driver
             Class.forName("org.postgresql.Driver");
         } catch (Exception e) {
 
         }
+        // header is required to send cross-origin requests
         resp.setHeader("Access-Control-Allow-Origin","*");
+        // set the response data type to JSON
         resp.setContentType("application/json");
+        // initialise the connection
         Connection conn = null;
         try {
+            // attempt connection
             conn = DriverManager.getConnection(dbURL, "hdzsfwizwsoxxz", "b62525ba2575aa2a67faab4ab24f80234d850305522338e2bb16dd02f474db8e");
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
+        // use getServletPath to extract where the request came from
         String path = req.getServletPath();
 
+        // perform switch with the path
         switch(path){
+
+            // if request came from homepage
             case "/":
                 try {
                     Statement s = conn.createStatement();
@@ -57,27 +68,38 @@ public class MyServlet extends HttpServlet {
         }
 
     }
+
+    // function for handling POST requests from frontend
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        // extract where request came from
         String path = req.getServletPath();
 
+        // database URL
         String dbURL = "jdbc:postgresql://ec2-54-195-252-243.eu-west-1.compute.amazonaws.com:5432/din8m6nuaj2gb?ssl" +
                 "=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
         try {
+            // connect to postgres driver
             Class.forName("org.postgresql.Driver");
         } catch (Exception e) {
 
         }
+        // set response data type to JSON
         resp.setContentType("application/json");
+        // intialise the connection
         Connection conn = null;
         try {
+            // attempt connection to database
             conn = DriverManager.getConnection(dbURL, "hdzsfwizwsoxxz", "b62525ba2575aa2a67faab4ab24f80234d850305522338e2bb16dd02f474db8e");
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
+        // perform switch with path
         switch(path){
+
+            // if request comes from new patient page
             case "/newpatient":
                 String reqBody=req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
                 JSONObject obj = new JSONObject(reqBody);
@@ -101,6 +123,8 @@ public class MyServlet extends HttpServlet {
                     System.out.println("FAILED");
                 }
                 break;
+
+            // if request comes from view page
             case "/viewpage":
                 try {
                     String reqBody2=req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
